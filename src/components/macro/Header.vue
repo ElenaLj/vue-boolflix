dd51bbfdf58fe002184ed2ff98690381757b2e7d
ElenaLj <elenaljubijankic@gmail.com>
2021-12-21 15:52:15 +0100

    init project

---
+++ src/components/macro/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <SearchBox/>
+  </div>
+</template>
+
+<script>
+import SearchBox from '../commons/SearchBox.vue';
+
+export default {
+    name: 'Header',
+    components: {
+      SearchBox
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
