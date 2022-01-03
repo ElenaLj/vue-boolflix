eec838832439eddd7c517334e18cc75c1248a795
ElenaLj <elenaljubijankic@gmail.com>
2022-01-03 12:17:52 +0100

    insert header

--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="search-box">
+  <div class="header">
+    <!-- logo -->
+    <div class="logo">
+      <h1>BOOLFLIX</h1>
+    </div>
+    <!-- /logo -->
+
+    <!-- navbar -->
+    <ul>
+      <li>Home</li>
+      <li>Serie TV</li>
+      <li>Film</li>
+      <li>Nuovi e popolari</li>
+      <li>La mia lista</li>
+      <li>Audio e sottotitoli</li>
+    </ul>
+    <!-- /navbar -->
+    <!-- searchbox -->
     <form @submit.prevent="$emit('search', searchText)">
       <input type="text" placeholder="Titolo film o serie TV" v-model="searchText">
       <button>Cerca</button>
     </form>
+    <!-- /searchbox -->
   </div>
 </template>
 
@@ -21,5 +39,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/style/global.scss";
 
 </style>
